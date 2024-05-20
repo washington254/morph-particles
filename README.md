ee8de24e1c224994009ced4e464e044080d015ba
washington maina <103877612+washington254@users.noreply.github.com>
2024-05-20 13:39:07 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,9 @@
+How to install and run this template? \
+It's simple, just run the following commands: 
+```
+npm i
+npm run dev
+```
+
+
+https://github.com/MisterPrada/morph-particles/assets/8146111/be440bfb-34f6-4bde-ba08-12b42877fd95
---
+++ src/Experience/sources.js
@@ -0,0 +1,55 @@
+export default [
+
+
+    {
+        name: 'e2Model',
+        type: 'gltfModel',
+        path: 'models/e2.glb'
+    },
+
+    {
+        name: 'boyModel',
+        type: 'gltfModel',
+        path: 'models/boy.glb'
+    },
+    {
+        name: 'danceModel',
+        type: 'gltfModel',
+        path: 'models/dancing.glb'
+    },
+    {
+        name: 'horseModel',
+        type: 'gltfModel',
+        path: 'models/horse.glb'
+    },
+    {
+        name: 'treeModel',
+        type: 'gltfModel',
+        path: 'models/tree.glb'
+    },
+    {
+        name: 'musicModel',
+        type: 'gltfModel',
+        path: 'models/music.glb'
+    },
+    {
+        name: 'gModel',
+        type: 'gltfModel',
+        path: 'models/g/g.gltf'
+    },
+    {
+        name: 'radioModel',
+        type: 'gltfModel',
+        path: 'models/radio/radio.gltf'
+    },
+    {
+        name: 'subModel',
+        type: 'gltfModel',
+        path: 'models/sub/scene.gltf'
+    },
+    {
+        name: 'dModel',
+        type: 'gltfModel',
+        path: 'models/d.glb'
+    },
+]
